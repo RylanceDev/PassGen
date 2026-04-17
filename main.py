486a01de2e157f12cc369cb98c72015ccd094d84
RylanceDev <safedaner@gmail.com>
2026-04-17 01:16:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from generator import generate_password
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "API activa. Usa /password o /docs"}
+
 @app.get("/password")
 def password(length: int = 16, symbols: bool = True):
     if length < 6 or length > 64:
